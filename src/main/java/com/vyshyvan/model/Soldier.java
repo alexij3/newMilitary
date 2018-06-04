24b71a6e338a1e1b31ce9cb035e058889a6c8acf
Alex Buzilov <slmk445@gmail.com>
2018-06-04 11:58:38 +0300

    Merge remote-tracking branch 'origin/postgresql' into postgresql

--- src/main/java/com/vyshyvan/model/Soldier.java
+++ src/main/java/com/vyshyvan/model/Soldier.java
@@ -18,7 +18,7 @@ public class Soldier {
     @Column
     private int age;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.DETACH)
     @JoinColumn(name = "military_base_id", nullable = true)
     @JsonIgnore
     private MilitaryBase militaryBase;
